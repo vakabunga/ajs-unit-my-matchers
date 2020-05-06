c5dd3d2cfdf3a962ab3da261e3ff48f873b370d4
Vakabunga <shesterov@gmail.com>
2020-05-06 22:24:48 +0300

    all tests done

--- src/js/__tests__/sortArmy.test.js
+++ src/js/__tests__/sortArmy.test.js
@@ -7,10 +7,32 @@ describe('sortArmy:', () => {
       { name: 'маг', health: 100 },
       { name: 'лучник', health: 80 },
     ]);
-    expect(result).toBe([
+    expect(result).toEqual([
       { name: 'маг', health: 100 },
       { name: 'лучник', health: 80 },
       { name: 'мечник', health: 10 },
+    ]);
+  });
+  test('should sort army from the highest health to the lowest', () => {
+    const result = sortArmy([
+      { name: 'мечник', health: 100 },
+      { name: 'маг', health: 100 },
+      { name: 'пращник', health: 50 },
+      { name: 'лучник', health: 0 },
+      { name: 'канонир', health: 100 },
+      { name: 'всадник', health: 15 },
+      { name: 'рыцарь', health: 45 },
+      { name: 'мушкетёр', health: 90 },
+    ]);
+    expect(result).toEqual([
+      { name: 'канонир', health: 100 },
+      { name: 'маг', health: 100 },
+      { name: 'мечник', health: 100 },
+      { name: 'мушкетёр', health: 90 },
+      { name: 'пращник', health: 50 },
+      { name: 'рыцарь', health: 45 },
+      { name: 'всадник', health: 15 },
+      { name: 'лучник', health: 0 },
     ]);
   });
 });
